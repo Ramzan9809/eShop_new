da7f2893312775d57eb76a121db0e56277dec074
Ramzan9809 <ramzan.rgwctx@gmail.com>
2025-04-14 19:37:47 +0600

    add model settings

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login as user_login, logout as user_logout, get_user_model
+from django.contrib.auth.decorators import login_required
+from apps.products.models import Category
 
-# Create your views here.
+User= get_user_model()
+
+def login_view(request):
+     categories = Category.objects.all()[:6]
+     if request.method == 'POST':
+         login = request.POST.get('login')
+         password = request.POST.get('password')
+         usr = authenticate(request, username=login, password=password)
+         if usr is not None: # проверка на сущ
+             user_login(request, usr)
+             return HttpResponseRedirect('/')
+         else:
+             return render(request, 'auth/login.html', {'error':'Неверный логин или пароль', 'categories':categories})
+     return render(request, 'auth/login.html', {'categories':categories})
+
+
+def reg_view(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password != password2:
+            return render(request, 'auth/reg.html', {'error': 'Пароли не совпадают'})
+
+        if len(password)<6:
+            return render(request, 'auth/reg.html', {'error': 'Пароли должен содержать больше 6 символов'})
+        
+        if password == password2:
+            User.objects.create_user(username=login, password=password)
+            usr = authenticate(request, username=login, password=password)
+            if usr is not None:
+                user_login(request, usr)
+                return HttpResponseRedirect('/')
+            else:
+                return render(request, 'auth/login.html', {'error':'Ошибка аутентификации'})
+    return render(request, 'auth/register.html')
--- eshop/urls.py
+++ eshop/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from apps.products.views import home, product_detail, category_detail, contact, content
+from apps.users.views import login_view, reg_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,6 +12,8 @@ urlpatterns = [
     path('product/<slug:slug>/', product_detail, name='product_detail'),
     path('category/<slug:slug>/', category_detail, name='category_detail'),
     path('content/', content, name='content'),
+    path('login/', login_view, name='login'),
+    path('register/', reg_view, name='register'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
